39c807960d1703164d7ee7ebd4aa101e57675aee
Shiun Robot DIY <masonchen1003@gmail.com>
2024-06-04 12:45:13 +0800

    Update screen1.cpp

--- screen1.cpp
+++ screen1.cpp
@@ -1,37 +1,19 @@
 namespace pxt {
+    // 声明接口函数
+    void setOffX(uint8_t x);
+    void setOffY(uint8_t y);
+}
 
-class WDisplay {
-  // existing code...
-
-  public:
+namespace pxt {
     void setOffX(uint8_t x) {
-        offX = x;
-        setAddrMain(); // 更新地址窗口
+        auto display = getWDisplay();
+        if (display)
+            display->setOffX(x);
     }
 
     void setOffY(uint8_t y) {
-        offY = y;
-        setAddrMain(); // 更新地址窗口
+        auto display = getWDisplay();
+        if (display)
+            display->setOffY(y);
     }
-};
-
-// existing code...
-// 声明接口函数
-void setOffX(uint8_t x);
-void setOffY(uint8_t y);
-
-// 定义接口函数
-void setOffX(uint8_t x) {
-    auto display = getWDisplay();
-    if (display)
-        display->setOffX(x);
 }
-
-void setOffY(uint8_t y) {
-    auto display = getWDisplay();
-    if (display)
-        display->setOffY(y);
-}
-
-}
-
