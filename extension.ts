6a25f38fe5f6eeb735fbb6daf273e829a611a55e
Shiun Robot DIY <masonchen1003@gmail.com>
2024-06-04 12:19:16 +0800

    Update extension.ts

--- extension.ts
+++ extension.ts
@@ -1,16 +1,21 @@
 namespace MyExtension {
 // 在 custom.ts 中新增以下内容
-
 //% blockId=display_set_offx block="set offX to %x"
 //% weight=100
 export function setOffX(x: number): void {
-    pxtrun.setOffX(x);
+    // 调用 C++ 中的 setOffX 函数
+    control.runInParallel(() => {
+        pxsim.pxtrt.setOffX(x);
+    });
 }
 
 //% blockId=display_set_offy block="set offY to %y"
 //% weight=100
 export function setOffY(y: number): void {
-    pxtrun.setOffY(y);
+    // 调用 C++ 中的 setOffY 函数
+    control.runInParallel(() => {
+        pxsim.pxtrt.setOffY(y);
+    });
 }
 
 }
