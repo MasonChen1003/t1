67ff1e1a09f571c5599b9aeee66f686ec436e22a
Shiun Robot DIY <masonchen1003@gmail.com>
2024-06-04 12:12:51 +0800

    Update screen1.cpp

--- screen1.cpp
+++ screen1.cpp
@@ -16,5 +16,22 @@ class WDisplay {
 };
 
 // existing code...
+// 声明接口函数
+void setOffX(uint8_t x);
+void setOffY(uint8_t y);
+
+// 定义接口函数
+void setOffX(uint8_t x) {
+    auto display = getWDisplay();
+    if (display)
+        display->setOffX(x);
+}
+
+void setOffY(uint8_t y) {
+    auto display = getWDisplay();
+    if (display)
+        display->setOffY(y);
+}
 
 }
+
